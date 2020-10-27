7dc25800b39d984e4af7e5fc8b925e747115c4ad
Sento <vicentxtv49@gmail.com>
2020-10-27 13:15:29 +0100

    Buscado el index.html, copiada su ruta y almacenada en /vagrant/www, eliminado la ruta principal donde estaba anteriormente y modificado el fichero provision.sh para poder funcionar bien la maquina

--- nginx/provision.sh
+++ nginx/provision.sh
@@ -1,2 +1,7 @@
-sudo apt-get update
-sudo apt-get install nginx
+apt-get  -y update
+apt-get install -y nginx
+service nginx start
+service nginx status
+cp -avr /usr/share/nginx/www /vagrant/www
+sudo rm -r /usr/share/nginx/www
+sudo ln -s /vagrant/www /usr/share/nginx
