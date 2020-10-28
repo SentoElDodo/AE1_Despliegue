b5e5c2e2e75fc22581a728a953fdb6ff03ed663b
Sento <vicentxtv49@gmail.com>
2020-10-28 11:55:30 +0100

    sincronizaciones con vagrant vinculadas

--- nginx/provision.sh
+++ nginx/provision.sh
@@ -1,5 +1,6 @@
 apt-get -y update
 apt-get install -y nginx
+cp -avr /usr/share/nginx/www/ /vagrant/www
 rm -rf /etc/nginx/sites-enabled
 cp -R /vagrant/sites-enabled /etc/nginx/
 service nginx start
