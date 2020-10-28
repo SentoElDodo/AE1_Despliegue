8dad42a94b72a396706f49d527f77559398758d5
Sento <vicentxtv49@gmail.com>
2020-10-28 11:28:28 +0100

    Fichero default editado cambiando la ruta, las carpetas redireccionadas y eliminadas las indicadas, y comprobado que todo lo anterior funciona rompiendo y creando de nuevo la máquina

--- nginx/provision.sh
+++ nginx/provision.sh
@@ -1,7 +1,9 @@
-apt-get  -y update
+apt-get -y update
 apt-get install -y nginx
+rm -rf /etc/nginx/sites-enabled
+cp -R /vagrant/sites-enabled /etc/nginx/
 service nginx start
-service nginx status
-cp -avr /usr/share/nginx/www /vagrant/www
-sudo rm -r /usr/share/nginx/www
-sudo ln -s /vagrant/www /usr/share/nginx
+
+
+
+
